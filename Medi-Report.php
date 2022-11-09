d77bf59fed73a159b4ea7df7ef67f093f02261ee
Me8848You <me8848you@gmail.com>
2022-11-09 23:13:12 +0545

    Error Detecting

--- Medi-Report.php
+++ Medi-Report.php
@@ -60,9 +60,10 @@ $result = mysqli_query($conn, $sql);
     }
 
     main {
-        /* background-image: url("./images/hospital.jpg"); */
-        /* background-position: center;
-      background-repeat: no-repeat; */
+      background-image: url(./images/hospital.jpg);
+      /* background-repeat: no-repeat; */
+      background-size: contain;
+      margin-top: 0px;
 
     }
 
@@ -99,7 +100,6 @@ $result = mysqli_query($conn, $sql);
                         <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
                     </a>
                     <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                        <li><a class="dropdown-item" href="#">New project...</a></li>
                         <li><a class="dropdown-item" href="#">Settings</a></li>
                         <li><a class="dropdown-item" href="#">Profile</a></li>
                         <li>
